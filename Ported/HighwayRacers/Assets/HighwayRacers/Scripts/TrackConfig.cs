86dc4f3b674ce531f5f50d01ee3af8f35f20aa67
Lilith Duncan <lilith.duncan@unity3d.com>
2022-05-16 14:53:13 -0700

    basic UI system

---
+++ Ported/HighwayRacers/Assets/HighwayRacers/Scripts/TrackConfig.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct TrackConfig : IComponentData
+{
+    public int numberOfCars;
+    public int highwaySize;
+    public bool dirty;
+}
+
+public struct TrackConfigMinMax : IComponentData
+{
+    public int minNumberOfCars;
+    public int maxNumberOfCars;
+    public int minHighwaySize;
+    public int maxHighwaySize;
+}
---
+++ Ported/HighwayRacers/Assets/HighwayRacers/Scripts/TrackConfigAuthoring.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class TrackConfigAuthoring : MonoBehaviour
+{
+    public int defaultNumberOfCars = 50;
+    public int defaultHighwaySize = 500;
+    public int minNumberOfCars = 0;
+    public int maxNumberOfCars = 500;
+    public int minHighwaySize = 115;
+    public int maxHighwaySize = 500;
+}
+
+public class TrackConfigBaker : Baker<TrackConfigAuthoring>
+{
+    public override void Bake(TrackConfigAuthoring authoring)
+    {
+        AddComponent(new TrackConfig
+        {
+            numberOfCars = authoring.defaultNumberOfCars,
+            highwaySize = authoring.defaultHighwaySize
+        });
+
+        AddComponent(new TrackConfigMinMax
+        {
+            minNumberOfCars = authoring.minNumberOfCars,
+            maxNumberOfCars = authoring.maxNumberOfCars,
+            minHighwaySize = authoring.minHighwaySize,
+            maxHighwaySize = authoring.maxHighwaySize
+        });
+    }
+}
